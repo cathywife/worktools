b7ffdda8c49b5e4628fe66dfa119faca76ed6791
liuyunzou <1325659373@qq.com>
2017-04-07 15:13:50 +0800

    1111

--- rsync_tools_tencent.sh
+++
@@ -1,4 +0,0 @@
-chmod +x *.sh
-ssh -i /nba/pem/nba2-tencent.pem root@115.159.56.242 "cd /nba/server/tools/ && tar -czvf tools.`date +%s`.tgz *.sh"
-scp  -i /nba/pem/nba2-tencent.pem -r *.sh *.js root@115.159.56.242:/nba/server/tools/
-ssh -i /nba/pem/nba2-tencent.pem root@115.159.56.242 "cd /nba/server/tools/ && chmod +x *.sh"
